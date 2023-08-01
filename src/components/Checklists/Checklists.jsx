1f73d754f9281d96528238c65d44e7a6d4f76edc
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-08-01 16:31:39 -0500

    Created Checklists component and added it to navigation, taking user to page on click.

---
+++ src/components/Checklists/Checklists.jsx
@@ -0,0 +1,10 @@
+// - Checklists COMPONENT-
+function Checklists() {
+    return(
+        <h1>On checklist page</h1>
+    )
+}; // * end Checklists
+
+
+// - EXPORTING Checklists COMPONENT -
+export default Checklists;
