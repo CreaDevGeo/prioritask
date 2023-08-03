1a35232d41b550d1e2ed7970939db367c322b97d
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-08-03 14:15:44 -0500

    In ChecklistItem component, added rank and more options(ellipsis) button. Did more CSS styling. Styling for checklist box, and buttons.

--- src/components/Checklists/ChecklistItem/ChecklistItem.jsx
+++ src/components/Checklists/ChecklistItem/ChecklistItem.jsx
@@ -6,13 +6,12 @@ import "../ChecklistItem/ChecklistItem.css";
 import PriorityItem from "../../PriorityItem/PriorityItem";
 // Components
 
-
 // - ChecklistItem COMPONENT -
 function ChecklistItem({ checklist }) {
-    // Priorities of checklist
-    const priorities = checklist.checklist_item_priorities;
-  
-    // * Logging
+  // Priorities of checklist
+  const priorities = checklist.checklist_item_priorities;
+
+  // * Logging
   console.log(`\nchecklist ${checklist.checklist_id} is:`, checklist);
   console.log(`priorities are:`, priorities);
   // Probably gotta store priorities in ChecklistItem objects as property values and store in indiviual redux states
@@ -20,17 +19,25 @@ function ChecklistItem({ checklist }) {
   // - RENDERING -
   return (
     <React.Fragment>
-      <div className="checklist-box">
-        <center>
-          <h2>Checklist {checklist.checklist_id}</h2>
-        </center>
+      <div className="checklist-item-box">
+        <header className="checklist-item-header">
+          <button className="rank-button" type="button">Rank: </button>
+          <center>
+            <h2>Checklist {checklist.checklist_id}</h2>
+          </center>
+          <button className="edit-button" type="button">
+            ⚪️⚪️⚪️
+          </button>
+        </header>
         {/* Map checklist, creating a component for each priority */}
         {/* Priorities */}
         <section className="priorities-container">
           {priorities.map((priority) => {
-            return <PriorityItem key={priority.priority_id} />;
+            return (
+              <PriorityItem key={priority.priority_id} priority={priority} />
+            );
           })}
-          </section>
+        </section>
       </div>
     </React.Fragment>
   );
--- src/components/PriorityItem/PriorityItem.jsx
+++ src/components/PriorityItem/PriorityItem.jsx
@@ -1,5 +1,11 @@
 // - PriorityItem COMPONENT -
-function PriorityItem() {} // - END PriorityItem COMPONENT -
+function PriorityItem({ priority }) {
+  // - RENDERING -
+  return (
+  <h3>Priority {priority.priority_number}</h3>
+  
+  );
+} // - END PriorityItem COMPONENT -
 
 // * Exporting PriorityItem component
 export default PriorityItem;
