a4ab3b316dd222864f757a108af82c20c7847b97
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-08-03 19:11:43 -0500

    Updated Priorities folder stucture, addind a Tasks subfolder holding all task-related components, each with their own folders. | In Priority Item removed conditional rendering. Now passing TasksList component into each priority. | TasksList component will map through and render TaskItem component. | TaskItem will render MUI styling elements of each task.

---
+++ src/components/Priorities/Tasks/TaskItem/TaskItem.jsx
@@ -0,0 +1,18 @@
+// - IMPORTING -
+import React from "react";
+
+// - TaskItem COMPONENT -
+function TaskItem() {
+  // - RENDERING -
+  // Conditional rendering of MUI content (Null task or object)
+  return (
+    <div>
+      {/* Add your desired content here */}
+      {/* For example, you can show a simple message */}
+      <p>This is a TaskItem component.</p>
+    </div>
+  );
+} // - END TaskItem COMPONENT -
+
+// * Exporting TaskItem component
+export default TaskItem;
