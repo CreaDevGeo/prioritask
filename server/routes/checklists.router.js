932356f8575912961bd393d3dcc879316066e9ef
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-08-04 14:59:01 -0500

    In ChecklistItem component, created a delete button that  on click dispatches and action to remove a checklist. | In checklists redux saga, created a listener and action saga to send a DELETE request the user's selected checklist. It then re-renders with fetch dispatch of checklists. Using action payload as dynamic url. | On server side checklist router, made a DELETE request with multiple queries targeting each table item that is a child of checklist that was selected, running each subsequent query in numerous .thens

--- server/routes/checklists.router.js
+++ server/routes/checklists.router.js
@@ -13,6 +13,7 @@ router.get("/:id", (req, res) => {
   const userID = req.params.id;
 
   // SQL Query for all checklists
+  // Created a structured object build in select query
   const queryText = `
     SELECT
     checklists.checklist_id,
@@ -66,11 +67,12 @@ GROUP BY
       res.send(result.rows);
     })
     .catch((error) => {
-      console.log("Failed to retrieve checklists! ", error);
+      console.log("Failed to retrieve checklists! Error is:", error);
       res.sendStatus(500);
     });
 }); // * end GET all user's checklists
 
+// * POST request for adding checklist of user that is logged in
 router.post("/", (req, res) => {
   // Declaring req.body as variable
   const userID = req.body.userID;
@@ -81,7 +83,6 @@ router.post("/", (req, res) => {
     VALUES ($1);
     `;
 
-  // * POST request for adding checklist of user that is logged in
   pool
     .query(queryText, [userID])
     .then((result) => {
@@ -89,7 +90,51 @@ router.post("/", (req, res) => {
       res.sendStatus(201);
     })
     .catch((error) => {
-      console.log("Failed to add checklist! ", error);
+      console.log("Failed to add checklist! Error is:", error);
+      res.sendStatus(500);
+    });
+});
+
+// * DELETE request of user's selected checklist
+router.delete("/:id/:checklist", (req, res) => {
+  // Declaring user's id as parameter
+  const userID = req.params.id;
+  // Declaring user's checklist id as parameter
+  const checklistID = req.params.checklist;
+
+  // Queries
+  // Query to remove todos from selected checklist
+  const deleteTodosQuery = `
+    DELETE FROM todos WHERE task_id IN (SELECT task_id FROM tasks WHERE priority_id IN (SELECT priority_id FROM priorities WHERE checklist_id = $1));
+  `;
+
+  // Query to remove todos from selected checklist
+  const deleteTasksQuery = `
+    DELETE FROM tasks WHERE priority_id IN (SELECT priority_id FROM priorities WHERE checklist_id = $1);
+  `;
+
+  // Query to remove todos from selected checklist
+  const deletePrioritiesQuery = `
+    DELETE FROM priorities WHERE checklist_id = $1;
+  `;
+
+  // Query to remove todos from selected checklist
+  const deleteChecklistQuery = `
+    DELETE FROM checklists WHERE checklist_id = $1 AND user_id = $2;
+  `;
+
+  // Running multiple queries in the pool query
+  pool
+    .query(deleteTodosQuery, [checklistID])
+    .then(() => pool.query(deleteTasksQuery, [checklistID]))
+    .then(() => pool.query(deletePrioritiesQuery, [checklistID]))
+    .then(() => pool.query(deleteChecklistQuery, [checklistID, userID]))
+    .then(() => {
+      console.log("DELETE request made to remove a checklist!");
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log("Failed to remove checklist! Error is:", error);
       res.sendStatus(500);
     });
 });
