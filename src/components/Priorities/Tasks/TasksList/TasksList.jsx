7a73c468bd20417ca1bd53928bdf648305b45f04
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-08-04 10:29:29 -0500

    In checklists router, updated get request query. Also made a POST request to datatbase to add new checklist | In AllChecklists, added functionality to button to dispatch an action to redux saga with user id payload. | In checklists sage, mage a post request, with user id to create a new checklist. Saga then dispatches action to fetch new checklists. | In PriotiyItem component, made a console log conditional for logging. In TaskList component, using prop of tasks and mapping through to create a new taskItem component. | TaskItem component is rendering MUI modals for each task that is created.

--- src/components/Priorities/Tasks/TasksList/TasksList.jsx
+++ src/components/Priorities/Tasks/TasksList/TasksList.jsx
@@ -4,27 +4,27 @@ import React from "react";
 import TaskItem from "../TaskItem/TaskItem";
 
 // - PriorityTasks COMPONENT -
-function TasksList() {
+function TasksList({tasks}) {
 
   // - RENDERING -
   {
     /* Map through an array of tasks, rendering a new component for each */
   }
-  // return (
-  //   <div>
-  //     {tasks === null || tasks.length === 0 ? (
-  //       <TaskItem />
-  //     ) : (
-  //       tasks.map((task) => {
-  //         return (
-  //           <div key={task.task_id}>
-  //             <TaskItem task={task} /> {/* Corrected this line */}
-  //           </div>
-  //         );
-  //       })
-  //     )}
-  //   </div>
-  // );
+  return (
+    <div>
+      {tasks === null || tasks.length === 0 ? (
+        <TaskItem />
+      ) : (
+        tasks.map((task) => {
+          return (
+            <div key={task.task_id}>
+              <TaskItem task={task} /> {/* Corrected this line */}
+            </div>
+          );
+        })
+      )}
+    </div>
+  );
 } // - END PriorityTasks COMPONENT -
 
 // * Exporting PriorityTasks component
