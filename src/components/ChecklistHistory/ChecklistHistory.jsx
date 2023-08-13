8eed32ce61a4b3925db01e44bd801907fdc28366
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-08-13 11:24:52 -0500

    Created an MUI table with dummy data and mapping function to display all new data that will come into the table. It will show the chcklist id, priority, priority completion date, tasks, and their completion date.

--- src/components/ChecklistHistory/ChecklistHistory.jsx
+++ src/components/ChecklistHistory/ChecklistHistory.jsx
@@ -1,8 +1,73 @@
+import React from "react";
+import {
+  Table,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Paper,
+} from "@mui/material";
+
+// Example dynamic data (replace this with your actual data)
+const checklistHistoryData = [
+  {
+    checklistID: 1,
+    priorities: [
+      { priority: "Priority 1", deadline: "Deadline 1" },
+      { priority: "Priority 2", deadline: "Deadline 2" },
+      { priority: "Priority 3", deadline: "Deadline 3" },
+    ],
+    prioritiesCompletedAt: "Priorities Completion Date",
+    tasks: ["Task 1", "Task 2", "Task 3"],
+    tasksCompletedAt: "Tasks Completion Date",
+  },
+  // Add more data entries as needed
+];
+
 // - ChecklistHistory COMPONENT -
 function ChecklistHistory() {
-    return(
-        <h1>In Checklist History</h1>
-    )
+  return (
+    <>
+      <center>
+        <h1>Checklist History</h1>
+      </center>
+      <TableContainer component={Paper} style={{ marginBottom: "16px" }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Checklist ID</TableCell>
+              <TableCell>Priorities and Deadlines</TableCell>
+              <TableCell>Priorities Completed At</TableCell>
+              <TableCell>Tasks</TableCell>
+              <TableCell>Tasks Completed At</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {checklistHistoryData.map((entry) => (
+              <TableRow key={entry.checklistID}>
+                <TableCell>{entry.checklistID}</TableCell>
+                <TableCell>
+                  {entry.priorities.map((priority) => (
+                    <div key={priority.priority}>
+                      {priority.priority}: {priority.deadline}
+                    </div>
+                  ))}
+                </TableCell>
+                <TableCell>{entry.prioritiesCompletedAt}</TableCell>
+                <TableCell>
+                  {entry.tasks.map((task) => (
+                    <div key={task}>{task}</div>
+                  ))}
+                </TableCell>
+                <TableCell>{entry.tasksCompletedAt}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
 } // - END ChecklistHistory COMPONENT -
 
 export default ChecklistHistory;
