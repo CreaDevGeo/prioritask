78231ea2abb7d506d3608855debd78cdf7dd6b45
Georgio Harris <123996128+CreaDevGeo@users.noreply.github.com>
2023-08-26 18:10:34 -0500

    Updated message in EmptyChecklists component.

--- src/components/Checklists/EmptyChecklists/EmptyChecklists.jsx
+++ src/components/Checklists/EmptyChecklists/EmptyChecklists.jsx
@@ -7,7 +7,7 @@ function EmptyChecklists() {
   return (
     <h2 className="empty-checklists-heading">
       You have no checklists. <br />
-      Start making planning some goals!
+      Start making some goals!
     </h2>
   );
 } // * - END EmptyChecklists COMPONENT -
